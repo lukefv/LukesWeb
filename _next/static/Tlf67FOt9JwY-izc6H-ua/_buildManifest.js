4dbea2fe6e08506d1b5a651546a89675b31b604a
lukefv <99335393+lukefv@users.noreply.github.com>
2023-06-11 10:22:18 +1000

    Add files via upload

---
+++ _next/static/Tlf67FOt9JwY-izc6H-ua/_buildManifest.js
@@ -0,0 +1,11 @@
+self.__BUILD_MANIFEST = {
+    __rewrites: {
+        beforeFiles: [],
+        afterFiles: [],
+        fallback: []
+    },
+    "/": ["static/chunks/68-4f6db2381878f755.js", "static/chunks/pages/index-e62cd423f11114e2.js"],
+    "/_error": ["static/chunks/pages/_error-54de1933a164a1ff.js"],
+    sortedPages: ["/", "/_app", "/_error"]
+},
+self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
